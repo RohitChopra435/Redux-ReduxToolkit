21e539a801ad1d368ecd2c4c2a449545c3bb7885
devops4sure <devops4sure@gmail.com>
2024-05-15 13:53:12 +0530

    first commit

---
+++ src/store/ui-slice.js
@@ -0,0 +1,20 @@
+import { createSlice } from "@reduxjs/toolkit";
+const initialState = { cartIsVisible: false, notification: null };
+const uiSlice = createSlice({
+  name: "ui",
+  initialState,
+  reducers: {
+    toggle(state) {
+      state.cartIsVisible = !state.cartIsVisible;
+    },
+    showNotification(state, action) {
+      state.notification = {
+        status: action.payload.status,
+        title: action.payload.title,
+        message: action.payload.message,
+      };
+    },
+  },
+});
+export default uiSlice.reducer;
+export const uiActions = uiSlice.actions;
